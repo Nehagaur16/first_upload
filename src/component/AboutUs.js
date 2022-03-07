c93b83cc708aa8a6f31a135eaffde29b68598976
neha-gaur <er.nehagaur16@gmail.com>
2022-03-07 17:45:28 +0530

    second-commit

--- src/component/AboutUs.js
+++ src/component/AboutUs.js
@@ -4,6 +4,9 @@ export default function AboutUs() {
     return (
         <div>
             <h1>About Us Page</h1>
+            <p>Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor 
+                sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+            </p>
         </div>
     )
 }
